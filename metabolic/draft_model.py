f057e119f9d7f354a43f5f003f05074bc75c70c9
scwatts <s.watts2@student.unimelb.edu.au>
2020-06-30 16:25:01 +1000

    Prepare data to troubleshoot failed draft models

--- metabolic/draft_model.py
+++ metabolic/draft_model.py
@@ -5,6 +5,7 @@ import tempfile
 
 
 import Bio.SeqIO
+import cobra.flux_analysis
 import cobra.io
 import cobra.manipulation
 import cobra.manipulation.modify
@@ -15,13 +16,13 @@ from . import media_definitions
 from . import util
 
 
-def run(assembly_fp, ref_gbk_fp, model, output_fp):
+def run(assembly_fp, ref_genbank_fp, model, output_fp):
     print('\n========================================')
     print('running draft model creation')
     print('========================================')
     # Get orthologs of genes in model
     model_genes = {gene.id for gene in model.genes}
-    isolate_orthologs = identify(assembly_fp, ref_gbk_fp, model_genes)
+    isolate_orthologs, blast_results = identify(assembly_fp, ref_genbank_fp, model_genes)
 
     # Remove genes from model that have no ortholog in the isolate
     missing_genes = list()
@@ -30,35 +31,84 @@ def run(assembly_fp, ref_gbk_fp, model, output_fp):
         if gene == 'KPN_SPONT':
             continue
         missing_genes.append(model.genes.get_by_id(gene))
-    # Mutate model inplace and rename genes
-    model.id = copy.deepcopy(assembly_fp.stem)
-    cobra.manipulation.remove_genes(model, missing_genes, remove_reactions=True)
-    cobra.manipulation.modify.rename_genes(model, isolate_orthologs)
+    # Mutate a copy of the model and rename genes
+    model_draft = copy.deepcopy(model)
+    model_draft.id = assembly_fp.stem
+    cobra.manipulation.remove_genes(model_draft, missing_genes, remove_reactions=True)
+    cobra.manipulation.modify.rename_genes(model_draft, isolate_orthologs)
 
     # Write model to disk
     with output_fp.open('w') as fh:
-        cobra.io.save_json_model(model, fh)
+        cobra.io.save_json_model(model_draft, fh)
 
     # Assess model by observing whether the objective function for biomass optimises
     # We perform an FBA on minimal media (m9)
-    for reaction in model.reactions:
+    for reaction in model_draft.reactions:
         if reaction.id.startswith('EX_'):
             reaction.lower_bound = 0
     for reaction_id, lower_bound in media_definitions.m9.items():
         try:
-            reaction = model.reactions.get_by_id(reaction_id)
+            reaction = model_draft.reactions.get_by_id(reaction_id)
         except KeyError:
             msg = f'warning: draft model {assembly_fp.stem} does not contain reaction {reaction_id}'
             print(msg, file=sys.stderr)
         reaction.lower_bound = lower_bound
-    solution = model.optimize()
+    solution = model_draft.optimize()
 
     # Somewhat arbitrary threshold for whether a model produces biomass
     if solution.objective_value < 1e-4:
-        # TODO: troubleshooting information if FBA fails to optimise to target
         msg = (f'error: model for {assembly_fp.stem} failed to produce biomass on minimal media, '
                 'manual intervention is required to fix the draft model')
         print(msg, file=sys.stderr)
+
+        # TODO: place this troubleshooting information in an output
+        gapfilled = cobra.flux_analysis.gapfill(model_draft, model, demand_reactions=False, iterations=5)
+        reaction_counts = dict()
+        for result in gapfilled:
+            for reaction in result:
+                if reaction not in reaction_counts:
+                    reaction_counts[reaction] = 0
+                reaction_counts[reaction] += 1
+        print('\n', 'Gapfilling results', sep='')
+        for reaction, count in reaction_counts.items():
+            print(reaction.id, count)
+
+        print('\n', 'BLASTp hits', sep='')
+        for reaction in reaction_counts:
+            for gene in reaction.genes:
+                hits = blast_results['blastp_ref'].get(gene.id, list())
+                print(reaction.id, gene.id, len(hits))
+
+        print('\n', 'BLASTn hits', sep='')
+        for reaction in reaction_counts:
+            for gene in reaction.genes:
+                hits = blast_results['blastn'].get(gene.id, list())
+                print(reaction.id, gene.id, len(hits))
+
+        with open('data/bigg_models_reactions.txt', 'r') as fh:
+            line_token_gen = (line.rstrip().split('\t') for line in fh)
+            header_tokens = next(line_token_gen)
+            records = dict()
+            for line_tokens in line_token_gen:
+                record = {k: v for k, v in zip(header_tokens, line_tokens)}
+                assert record['bigg_id'] not in records
+                records[record['bigg_id']] = record
+
+        print('\n', 'BiGG info', sep='')
+        for reaction in reaction_counts:
+            record = records[reaction.id]
+            print('name: ', reaction.id, sep='')
+            print('reaction:', record['reaction_string'])
+            print('urls:')
+            for url_entry in record['database_links'].split('; '):
+                print('\t', url_entry, sep='')
+            print()
+
+        # Other information about these genes
+        #       - is the gene ORF complete
+        #           - see genbank notes if reannotated
+        #           - check if at bounds
+
         sys.exit(1)
     else:
         # TODO: report in more meaningful way
@@ -71,13 +121,13 @@ def run(assembly_fp, ref_gbk_fp, model, output_fp):
 
 def identify(iso_fp, ref_fp, model_genes):
     #pickle_mode = 'read'
-    #pickle_mode = 'write'
-    pickle_mode = 'noop'
+    pickle_mode = 'write'
+    #pickle_mode = 'read'
     # First we perform a standard best bi-directional hit analysis to identify orthologs
     # Extract protein sequences from both genomes but only keep model genes from the reference
-    dh = tempfile.TemporaryDirectory()  # fs directory deleted this is out of scope
-    iso_protein_fp = util.write_gbk_sequence(iso_fp, dh.name, seq_type='prot')
-    ref_protein_fp = util.write_gbk_sequence(ref_fp, dh.name, model_genes, seq_type='prot')
+    dh = tempfile.TemporaryDirectory()
+    iso_protein_fp = util.write_genbank_coding_sequence(iso_fp, dh.name, seq_type='prot')
+    ref_protein_fp = util.write_genbank_coding_sequence(ref_fp, dh.name, model_genes, seq_type='prot')
     # Run BLASTp bidirectionally (filtering with evalue <=1e-3, coverage >=25%, and pident >=80%)
     blastp_iso = alignment.run_blastp(iso_protein_fp, ref_protein_fp, dh.name)
     blastp_ref = alignment.run_blastp(ref_protein_fp, iso_protein_fp, dh.name)
@@ -85,14 +135,14 @@ def identify(iso_fp, ref_fp, model_genes):
     # TEMP: store/load blastp results to/from disk so we dont have to recompute
     import pickle
     if pickle_mode == 'write':
-        with open('pickled/blastp_iso.bin', 'wb') as fh:
+        with open(f'pickled/{iso_fp.stem}_blastp_iso.bin', 'wb') as fh:
             pickle.dump(blastp_iso, fh)
-        with open('pickled/blastp_ref.bin', 'wb') as fh:
+        with open(f'pickled/{iso_fp.stem}_blastp_ref.bin', 'wb') as fh:
             pickle.dump(blastp_ref, fh)
     elif pickle_mode == 'read':
-        with open('pickled/blastp_iso.bin', 'rb') as fh:
+        with open(f'pickled/{iso_fp.stem}_blastp_iso.bin', 'rb') as fh:
             blastp_iso = pickle.load(fh)
-        with open('pickled/blastp_ref.bin', 'rb') as fh:
+        with open(f'pickled/{iso_fp.stem}_blastp_ref.bin', 'rb') as fh:
             blastp_ref = pickle.load(fh)
     elif pickle_mode == 'noop':
         pass
@@ -116,8 +166,8 @@ def identify(iso_fp, ref_fp, model_genes):
     # Get reference genes that do not have an ortholog
     model_genes_no_orth = model_genes.difference(set(model_orthologs))
     # Extract gene sequences from reference and write isolate sequence as fasta
-    ref_gene_fp = util.write_gbk_sequence(ref_fp, dh.name, model_genes_no_orth, seq_type='nucl')
-    iso_fasta_fp = util.write_gbk_to_fasta(iso_fp, dh.name)
+    ref_gene_fp = util.write_genbank_coding_sequence(ref_fp, dh.name, model_genes_no_orth, seq_type='nucl')
+    iso_fasta_fp = util.write_genbank_to_fasta(iso_fp, dh.name)
     # Run BLASTn (filtering with evalue <=1e-3, coverage >=80%, and pident >=80%)
     blastn_res = alignment.run_blastn(ref_gene_fp, iso_fasta_fp, dh.name)
 
@@ -137,4 +187,7 @@ def identify(iso_fp, ref_fp, model_genes):
             else:
                 model_orthologs[ref_gene_name] = f'{hit.qseqid}_unannotated'
                 break
-    return model_orthologs
+
+    # Return orthologs and BLAST results
+    blast_results = {'blastp_iso': blastp_iso, 'blastp_ref': blastp_ref, 'blastn': blastn_res}
+    return model_orthologs, blast_results
