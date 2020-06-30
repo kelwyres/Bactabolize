7d7ee7bb9b66f0995f83f005cd3dda86c90ed115
scwatts <s.watts2@student.unimelb.edu.au>
2020-06-30 16:24:11 +1000

    Clean up full workflow execution

--- metabolic/__main__.py
+++ metabolic/__main__.py
@@ -23,8 +23,8 @@ def entry():
     elif args.command == 'annotate':
         annotate.run(args.assembly_fp, args.prodigal_model_fp, args.output_fp)
     elif args.command == 'draft_model':
-        model = util.read_model_and_check(args.ref_model_fp, args.ref_gbk_fp)
-        draft_model.run(args.assembly_fp, args.ref_gbk_fp, model, args.output_fp)
+        model = util.read_model_and_check(args.ref_model_fp, args.ref_genbank_fp)
+        draft_model.run(args.assembly_fp, args.ref_genbank_fp, model, args.output_fp)
     elif args.command == 'model_fba':
         model_fba.run(args.model_fp)
     else:
@@ -32,26 +32,40 @@ def entry():
 
 
 def run_complete_workflow(args):
-    # TODO determine input assembly filetype and handle, currently assuming genbank
-    dh = tempfile.TemporaryDirectory()  # fs directory deleted this is out of scope
-    assembly_fasta_fp = util.write_gbk_to_fasta(args.assembly_fp, dh.name)
-    # TODO: apply some QC to these stats
-    if not args.no_qc:
-        # TODO: using named arg to show that we need to set output_fp to something later
-        assembly_stats.run(assembly_fasta_fp, output_fp=None)
-    # TODO: clean this up - don't use assembly_fp as a name for easier op below
-    if not args.no_annotation:
-        assembly_fp = pathlib.Path(dh.name, f'{args.assembly_fp.stem}_reanno.gbk')
-        annotate.run(assembly_fasta_fp, args.prodigal_model_fp, assembly_fp)
+    dh = tempfile.TemporaryDirectory()
+    assembly_filetype = util.determine_assembly_filetype(args.assembly_fp)
+    if assembly_filetype == 'genbank':
+        assembly_fasta_fp = util.write_genbank_to_fasta(args.assembly_fp, dh.name)
+    elif assembly_filetype == 'fasta':
+        assembly_fasta_fp = args.assembly_fp
     else:
-        assembly_fp = args.assembly_fp
+        assert False
+
+    # If we have a FASTA input, require that we annotate
+    if assembly_filetype == 'fasta' and args.no_reannotation:
+        print('error: cannot specify --no_reannotation with a FASTA input assembly', file=sys.stderr)
+        exit(1)
+
+    if not args.no_qc:
+        stats_fp = args.output_dir / f'{assembly_fasta_fp.stem}_stats.tsv'
+        assembly_stats.run(assembly_fasta_fp, stats_fp)
+
+    if not args.no_reannotation:
+        assembly_genbank_fp = pathlib.Path(args.output_dir, f'{args.assembly_fp.stem}_reannotated.gbk')
+        annotate.run(assembly_fasta_fp, args.prodigal_model_fp, assembly_genbank_fp)
+    else:
+        assembly_genbank_fp = args.assembly_fp
+
+    # Relate new annotations to existing ones, update reannotated genbank with existing information
+    if assembly_filetype and not args.no_reannotation:
+        annotate.match_existing_orfs_updated_annotations(assembly_genbank_fp, args.assembly_fp)
+
     # Create draft model
-    draft_model_fp = pathlib.Path(args.output_dir / f'{assembly_fp.stem}_model.json')
-    model = util.read_model_and_check(args.ref_model_fp, args.ref_gbk_fp)
-    draft_model.run(assembly_fp, args.ref_gbk_fp, model, draft_model_fp)
+    draft_model_fp = pathlib.Path(args.output_dir / f'{args.assembly_fp.stem}_model.json')
+    model = util.read_model_and_check(args.ref_model_fp, args.ref_genbank_fp)
+    draft_model.run(assembly_genbank_fp, args.ref_genbank_fp, model, draft_model_fp)
     # Simulate growth on media
     model_fba.run(draft_model_fp)
-
 
 
 if __name__ == '__main__':
