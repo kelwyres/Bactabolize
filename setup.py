96a35d73ea4e4695af3439fda140960207c7fe2b
scwatts <s.watts2@student.unimelb.edu.au>
2020-08-04 09:38:30 +1000

    Remove code and data for metabolite formulas

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ import metabolic
 
 
 setuptools.setup(
-        name='metabolic',
-        version=metabolic.__version__,
-        description='metabolic python package',
-        author='Stephen Watts',
-        license='GPLv3',
-        test_suite='tests',
-        packages=setuptools.find_packages(),
-        package_data={'metabolic': ['data/compound_id_map.tsv', 'data/formula.tsv.gz']},
-        entry_points={
-                'console_scripts': ['metabolic=metabolic.__main__:entry'],
-            }
+    name='metabolic',
+    version=metabolic.__version__,
+    description='metabolic python package',
+    author='Stephen Watts',
+    license='GPLv3',
+    test_suite='tests',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['metabolic=metabolic.__main__:entry'],
+    }
 )
