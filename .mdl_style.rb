f7b4ad7c82c9456dd5e0242c44db5d33675c10bf
Stephen Watts <hello@stephen.ac>
2022-06-07 18:02:18 +1000

    Add GH Actions, pre-commit hooks, versionbump

---
+++ .mdl_style.rb
@@ -0,0 +1,4 @@
+all
+exclude_rule 'MD033' # Allow inline HTML
+exclude_rule 'MD041' # Do not require first line to be top level header
+rule 'MD013', :line_length => 120 # Set line max length to 120
